dbfee021039f5c41f584e7708495ccfc812ad64a
jacob17 <jakob17chen@gmail.com>
2022-09-22 10:49:18 -0400

    hw3

--- solution-hw3/src/App.js
+++ solution-hw3/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Itemcard from './itemcard'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <img class="logo" src={process.env.PUBLIC_URL + "/assets/logo-01.svg"} alt="logo" />
+        <div class="navbar">
+          <div class="nav-item active">Products</div>
+          <div class="nav-item">Cart</div>
+        </div>
+        <div class="cart-summary">
+          <span class="cart-count">0 item</span><br />
+          <span class="cart-total">$ 0</span>
+        </div>
+        <div class="title">Our hand-made cinnamon rolls</div>
+      </nav>
+      <main>
+        <Itemcard
+          index={0} />
+        <Itemcard
+          index={1} />
+        <Itemcard
+          index={2} />
+        <Itemcard
+          index={3} />
+        <Itemcard
+          index={4} />
+        <Itemcard
+          index={5} />
+      </main>
+      <div class="alert">
+
+      </div>
     </div>
   );
 }
